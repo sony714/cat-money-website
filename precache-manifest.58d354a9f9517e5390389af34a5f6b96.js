c53fd60fd55e88d460bff5953f78162e14750877
meng <1059093241@qq.com>
2021-04-19 15:08:16 +0800

    deploy

---
+++ precache-manifest.58d354a9f9517e5390389af34a5f6b96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e5f4604a446e8def49a",
+    "url": "/cat-money-website/css/app.0bb2cda2.css"
+  },
+  {
+    "revision": "ff8ffd64742ce56773eaa9c9e3935353",
+    "url": "/cat-money-website/index.html"
+  },
+  {
+    "revision": "9e5f4604a446e8def49a",
+    "url": "/cat-money-website/js/app.bb91fc67.js"
+  },
+  {
+    "revision": "aea5c235981266a066bb",
+    "url": "/cat-money-website/js/chunk-vendors.c4103501.js"
+  },
+  {
+    "revision": "e588bd60ce0b74f1055635aeb81b8f97",
+    "url": "/cat-money-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/cat-money-website/robots.txt"
+  }
+]);
